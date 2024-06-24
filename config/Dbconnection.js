5e637a58eab079845125f1df00967afddacd17d5
ajith1506 <ajithkumararun1111@gmail.com>
2024-06-24 09:16:40 +0530

    change

--- config/Dbconnection.js
+++ config/Dbconnection.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 // Connect to MongoDB
 export const connectDB = async () => {
-  await mongoose.connect(process.env.DB_HOST);
+  await mongoose.connect(
+    `mongodb+srv://ajithkumararun1111:cUOpbsv0Y2fomPPA@123456.e47oxpw.mongodb.net/`
+  );
   console.log("MongoDB Connected...");
 };
